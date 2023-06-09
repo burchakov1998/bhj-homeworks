699d175001d24124d57f6727d54d48f5784cfa13
burchakov1998 <burchakov1998@gmail.com>
2023-06-09 15:01:42 +0300

    modifed

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,38 +1,41 @@
-const pollTitle = document.querySelector('#poll_title');
-const pollList = document.querySelector('poll_answers');
+
+const pollTitle = document.querySelector("#poll__title");
+const pollList = document.querySelector("#poll__answers");
 
 const getPollResult = async (voidId, index) => {
-     const response = await fetch('https://students.netoservices.ru/nestjs-backend/poll',{
-        method: 'POST',
-        Headers: {
-            'Content-type': 'application/x-www-form-urlencoded'
-        },
-        body: `vote=${voidId}&answer=${index}`
-});
-const {stat} = await response.json();
-pollList.textContent = '';
-let totalVotes = 0;
-for ({votes} of stat) {
-    totalVotes += votes;
-    
-}    
-for (const{answers, votes} of stat){
-    pollList.innerHTML += `${answers}: <strong>${(votes * 100 / totalVotes).toFixed(0)}</strong>%</br>`
-    alert('Спасибо,ваш голос засчитан!');
+//      const response = await
+//       fetch("https://students.netoservices.ru/nestjs-backend/poll",
+//       {
+//         method: "POST",
+//         Headers: {
+//             "Content-type": "application/x-www-form-urlencoded"
+//         },
+//         body: `vote=${voidId}&answer=${index}`
+// });
+// const {stat} = await response.json();
+// pollList.textContent = "";
+// let totalVotes = 0;
+// for ({votes} of stat) {
+//     totalVotes += votes;
+// }    
+// for (const{answers, votes} of stat){
+//     pollList.innerHTML += `${answers}: 
+//     <strong>${(votes * 100 / totalVotes).toFixed(0)}</strong>%</br>`  
+// };
+alert("Спасибо,ваш голос засчитан!");
 }
-
-async() => {
-    const response = await fetch('https://students.netoservices.ru/nestjs-backend/poll');
+(async() => {
+    const response = await fetch("https://students.netoservices.ru/nestjs-backend/poll");
     const body = await response.json();
     pollTitle.textContent = body.data.title;
-    body.data.answers.array.forEach((item,index) => {
-        const pollAnswer = document.createElement('button');
-        pollAnswer.classList.add('poll_answer');
-        pollAnswer.style.margin = '3px';
+    body.data.answers.forEach((item,index) => {
+        const pollAnswer = document.createElement("button");
+        pollAnswer.classList.add("poll__answers");
+        pollAnswer.style.margin = "3px";
         pollAnswer.textContent = item;
-        pollAnswer.addEventListener('click', () => {
+        pollAnswer.addEventListener("click", () => {
             getPollResult(body.id, index);
         });
 pollList.append(pollAnswer);
     });
-}};
+})();
---
+++ async-requests/preloader/task.js
@@ -0,0 +1,48 @@
+const loader = document.getElementById('loader');
+const itemContainer = document.getElementById('items');
+
+function showLoader(){
+    loader.classList.add('loader_active');
+}
+
+function hideLoader(){
+    loader.classList.remove('loader_active');
+}
+
+function displayItems(data){
+itemContainer.innerHTML = '';
+
+for (const currencyCode in data) {
+    if(data.hasOwnProperty(currencyCode)){
+        const currency = data[currencyCode];
+        const item = document.createElement('div');
+item.className = 'item';
+item.innerHTML = `
+<div class ='item__code'>${currency.CharCode}</div>
+<div class ='item__value'>${currency.Value}</div>
+<div class ='item__currency'>руб.</div>`
+itemContainer.appendChild(item);
+    }
+}
+}
+
+function loadCurrency(){
+showLoader();
+ //const cacheData = localStorage.getItem('currencyData');
+//  if(cacheData){
+//     const data = JSON.parse(cacheData);
+//     hideLoader();
+//     displayItems();
+//  }
+ fetch('https://students.netoservices.ru/nestjs-backend/slow-get-courses'
+ )
+ .then(Response => Response.json())
+ .then(data => {
+    localStorage.setItem('currencyData', JSON.stringify(data.Response));
+    hideLoader();
+    displayItems(data.Response.Valute);
+ })
+ .catch(error => console.error(error));
+}
+
+loadCurrency();
